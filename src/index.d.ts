b3ad8ef845e46a5b006c9873feae191582d4fdc2
Hoang Lam <hoanglam10499@gmail.com>
2022-06-14 20:14:16 +0700

    update 0.2.0

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,9 @@
 declare module 'react-native-input-accessory-view' {
   import { Component } from 'react';
-  import { StyleProp, ViewStyle } from 'react-native';
-  
   export interface InputAccessoryViewProps {
     animationInConfig?: object;
     animationOutConfig?: object;
-    renderView?: () => void;
-    isOpen?: boolean;
-    type?: string;
+    isVisible?: boolean;
   }
-
   export default class InputAccessoryView extends Component<InputAccessoryViewProps> {}
 }
